71f50a6875488d6c1577499c41df6fe13ff3ed54
namanmahna2 <namanmahna2@gmail.com>
2026-08-04 16:32:14 +0100

    bug fix

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,36 +3,28 @@ import type React from "react";
 import Header from "../components/header";
 import { Outlet } from "react-router-dom";
 import HeaderFooter from "../components/headerFooter";
-import { useEffect, useState } from "react";
-import { HeaderFooterContext } from "../helper/headerFooterContext";
 import { useLocation } from "react-router-dom";
 
 const DashBoard: React.FC = () => {
     const location = useLocation()
-    const [disabled, setDisabled] = useState(false);
 
-    useEffect(() => {
-        if (location.pathname === "/") setDisabled(false)
-    }, [location.pathname])
+    // useEffect(() => {
+    //     if (location.pathname === "/") setDisabled(false)
+    // }, [location.pathname])
 
     return (
-        <HeaderFooterContext.Provider value={{
-            disabled,
-            setDisabled
+        <Box sx={{
+            height: "100dvh",
+            display: "flex",
+            flexDirection: "column"
         }}>
-            <Box sx={{
-                height: "100dvh",
-                display: "flex",
-                flexDirection: "column"
-            }}>
-                <Header />
-                <hr />
-                {disabled && <HeaderFooter />}
-                <Box sx={{ flex: 1, overflow: "auto", p: 2 }}>
-                    <Outlet />
-                </Box>
-            </Box >
-        </HeaderFooterContext.Provider>
+            <Header />
+            <hr />
+            {<HeaderFooter />}
+            <Box sx={{ flex: 1, overflow: "auto", p: 2 }}>
+                <Outlet />
+            </Box>
+        </Box >
     )
 }
 
